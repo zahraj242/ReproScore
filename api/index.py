68b0e8cfa0b66ef3570b8732fbd192b23eed14af
zahraj242 <61913000+zahraj242@users.noreply.github.com>
2025-10-14 13:28:41 +0330

    Delete api/index.py

--- api/index.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-from streamlit.web import cli as stcli
-if __name__ == '__main__':
-sys.argv = ["streamlit", "run", "--server.port", os.environ.get('PORT', '8080'), "--server.headless", "true", "--server.enableCORS", "false", "your_script_name.py"]  # Replace with your Streamlit file, e.g., app.py
